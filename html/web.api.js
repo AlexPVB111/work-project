6c52a8ba0fff73408392e10b3f6278e4413925a8
alex <alexpvb111@yandex.ru>
2015-06-02 00:05:13 +0300

    get version

--- html/web.api.js
+++ html/web.api.js
@@ -1,6 +1,8 @@
 var api = {
 
-    currentHost: "http://localhost:80",
+    currentHost: //"http://localhost:80"
+	"http://localhost"
+	,
 
     /* пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ XMLHTTP */
     getXmlHttp: function() {
@@ -24,24 +26,38 @@ var api = {
     
     //пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ
     connect : function (host, callbackFunction) {
+		alert("connect");
+	
         if (host != "")
-            this.currentHost = host;
-        
+ 			this.currentHost = host;
+			
         var xmlhttp = this.getXmlHttp();
         xmlhttp.open('POST', this.currentHost + '/web/connect', true); // пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
         xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
         xmlhttp.send(null);
         xmlhttp.onreadystatechange = function() {
+			//alert('in readyState is -> ' + xmlhttp.readyState);
             if (xmlhttp.readyState == 4) {
+			
                 if (xmlhttp.status == 200) {
-                    callbackFunction();
+					alert("in connect");
+                    callbackFunction('ok');
                 }
+	//			else{
+	//				alert('I can\'t connect to Pilot-Server! ' + xmlhttp.status);
+    //                callbackFunction('I can\'t connect to Pilot-Server! Xmlhttp.status == ' + xmlhttp.status);
+	//			}
             }
-        };
+	//		else {
+	//			alert('Error! Xmlhttp.readyState is -> ' +  xmlhttp.readyState);
+    //            callbackFunction('I can\'t connect to Pilot-Server!  Xmlhttp.readyState == ' + xmlhttp.readyState);
+	//		}
+		};
     },
 
     openDatabase : function (database, login, password, useWinAuth, callbackFunction){
 		
+		alert("open db1");
         // construct an HTTP request
         var data = {
             "api" : "IServerApi",
@@ -51,28 +67,53 @@ var api = {
             "protectedPassword" : password,
             "useWindowsAuth" : useWinAuth
         };
-	
+	//этот alert уже не отрабатывает
+		alert("open db before call");
         var xmlhttp = this.getXmlHttp();
         xmlhttp.open('POST', this.currentHost + '/web/call', true);
         xmlhttp.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
         xmlhttp.send(JSON.stringify(data));
 		xmlhttp.onreadystatechange = function() {
+			alert("open db2");
             if (xmlhttp.readyState == 4) {
+				alert("open db3");
                 if (xmlhttp.status == 200) {
+					alert("open db4");
                     callbackFunction(xmlhttp.responseText);
                 }
-            }
+				/*
+				else{
+					alert('I can\'t open base! Xmlhttp.status == ' + xmlhttp.status);
+					callbackFunction('Error! I can\'t open base! Xmlhttp.status == ' + xmlhttp.status);
+				}
+				*/
+            }/*
+			else{
+				alert('Error! Xmlhttp.readyState is -> ' +  xmlhttp.readyState);
+				callbackFunction('Error! I can\'t open base! Xmlhttp.readyState == ' + xmlhttp.readyState);
+			}
+			*/
         };
     },
     
     connectToDatabase : function (host, database, login, password, useWinAuth, callbackFunction) {
 	
-		
+		alert("cdb1");
         var self = this;
-        this.connect(host, function () {
+        this.connect(host, function (result) {
 			
+			alert("cdb2");
 			
-            self.openDatabase(database, login, password, useWinAuth, callbackFunction);
+			if(result == 'ok'){
+			
+				alert("cdb3");
+				self.openDatabase(database, login, password, useWinAuth, callbackFunction);
+				}
+			else{
+			
+				alert("cdb4");
+				callbackFunction(result);
+				}
         });
     },
     
@@ -95,10 +136,40 @@ var api = {
                 }
             }
         };
-    }
+    },
 	
+    getVersion: function (callbackFunction)
+    {
 
+		// (1)
+		var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
+		var xhr = new XHR();
+		// (2) запрос на другой домен 
+		xhr.open('GET', this.currentHost + '/web/version', true);
+        xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');        
+		//xhr.setRequestHeader('Accept', 'text/html, application/xhtml+xml, */*');
+		//xhr.setRequestHeader('Accept-Language', 'ru-RU');
+		//xhr.setRequestHeader('Accept-Encoding','qzip, deflate');
 
-  
+		//сейчас только выводит в виде сообщения, надо доделать для отображения и обработать ошибки
+		xhr.onload = function() {
+		  //alert( this.responseText );
+		  if(this.status == 200){
+			callbackFunction( 'Connection to ' + this.responseText );
+		  }
+		  else
+			callbackFunction( 'I can\'t connect to Pilot-Server ' + this.responseText );
+		}
+
+		/*
+		xhr.onerror = function() {
+		  callbackFunction( 'Error!' + this.status );
+		}
+		*/
+		
+		xhr.send();
+		
+    }
+
 	
 };
--- html/web.control.js
+++ html/web.control.js
@@ -3,34 +3,61 @@ var control = {
     connectToDatabase : function (host, database, login, password, useWinAuth) {
 
         api.connectToDatabase(host, database, login, password, useWinAuth, function(result){
-
-			if(result.indexOf('not found') == -1){
-				if(result.indexOf('incorrect') == -1){
-					document.getElementById('connection').innerHTML = 'Connected';
-					
-					var databaseInfo = JSON.parse(result);
-                    var dbv = databaseInfo.DatabaseVersion;
-                    var bdid = databaseInfo.DatabaseId;
-                    var person = databaseInfo.Person.DisplayName;	
-					
-					//передача в качестве параметров: версии дб, id дб, имени пользователя
-					document.location.href = "html/main.html?par0="+dbv+"&par1="+bdid+"&par2="+person;
+			alert("result is -> " + result);
+			
+			if(result.indexOf('license limit') == -1){
+				if(result.indexOf('I can\'t connect') == -1){
+				
+					if(result.indexOf('not found') == -1){
+						if(result.indexOf('incorrect') == -1){
+							document.getElementById('connection').innerHTML = 'Connected';
+							alert("Final result is \n " + result);
+							var databaseInfo = JSON.parse(result);
+							var dbv = databaseInfo.DatabaseVersion;
+							var bdid = databaseInfo.DatabaseId;
+							var person = databaseInfo.Person.DisplayName;	
+							
+							//передача в качестве параметров: версии дб, id дб, имени пользователя
+							document.location.href = "html/main.html?par0="+dbv+"&par1="+bdid+"&par2="+person;
+						}
+						else{
+							alert("User parameters is wrong!");
+							alert(result);
+							document.getElementById('connection').innerHTML = '';
+							//document.getElementById('wrong').innerHTML = 'Incorrected parameters.Use others.';
+							document.getElementById('wrong').innerHTML = result;
+							//document.getElementById('mainframe').reset();
+						}
+					}	
+					else{
+						alert("Datebase not found!");
+						document.getElementById('connection').innerHTML = '';
+						//document.getElementById('wrong').innerHTML = 'Date base not found. Use other.';
+						document.getElementById('wrong').innerHTML = result;
+					}
+				
 				}
 				else{
-					alert("User parameters is wrong!");
 					document.getElementById('connection').innerHTML = '';
-					document.getElementById('wrong').innerHTML = 'Incorrected parameters.Use others.';
-					document.getElementById('mainframe').reset();
+					document.getElementById('wrong').innerHTML = result;
 				}
-			}	
-			else{
-				alert("Datebase not found!");
-				document.getElementById('connection').innerHTML = '';
-				document.getElementById('wrong').innerHTML = 'Date base not found. Use other.';
 			}
+			else{
+				document.getElementById('connection').innerHTML = '';
+				document.getElementById('wrong').innerHTML = result;
+			}
+			
 		
 		});
-    }
+    },
+	
+	getVersion: function(){
+	
+				api.getVersion(function(result){
+					document.getElementById('pilotversion').innerHTML = result;
+				});
+	
+	}
 	
 	 
 
